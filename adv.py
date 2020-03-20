8c8a34e6d51e0a5a40f2ac979bdf1eaa3f12ea3a
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-03-19 18:28:34 -0700

    began basic explore_all_rooms func with dft and bfs written out to use

--- adv.py
+++ adv.py
@@ -5,16 +5,18 @@ from world import World
 import random
 from ast import literal_eval
 
+from util import Stack, Queue
+
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -25,11 +27,75 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
+
+def explore_all_rooms(player, traversal_path):
+    # Fill this out with directions to walk: ['n', 'n']
+    traversal_path = []
+    starting_room = player.current_room
+
+    # player.current_room.id
+    # player.current_room.get_exits()
+    # player.travel(direction)
+
+    def get_adj_rooms(self, starting_room): # current_room.get_exits() is same?
+        if starting_room in self.rooms:
+            return self.rooms[starting_room]
+        else:
+            print('Error: this room does not exist.')
+            raise ValueException
 
 
+    def bfs(self, starting_room, dest_point): #player.current_room.id
+        # create queue
+        q = Queue()
+        # enqueu path to start
+        q.append(starting_room)
+        # create a set? dict? to store visited rooms
+        visited = {}
+        # while queue is full...
+        while q.size() > 0:
+            #dequeue first path
+            path = q.dequeue()
+            # grab room from end
+            room = path[-1]
+            # check if visited. if not...
+            if room not in visited:
+                #mark as visited
+                visited.add(room)
+                # check if is target: ? 
+                if room == dest_point:
+                    # dft(room) -- this go here?
+                    return room
+                #enqueu path to its neigbors
+                for adj_rooms in self.get_adj_rooms(room):
+                    path_copy = path.copy()
+                    path_copy.enqueue()
+
+
+    def dft(self, starting_room): 
+        #create stack
+        s = Stack()
+        #push starting v
+        s.push([starting_room]) #add .directions here?
+        #create path for visited
+        visited = {}
+        #while stack is full:
+        while s.size() > 0:
+            #pop first vertx and check if visited
+            path = s.pop()
+            room_id = path[-1]
+            if room_id not in visited:
+                #mark as visited
+                visited[room_id] = path
+                #push neighbors onto stack
+                for adj_rooms in self.get_adj_rooms[room_id]:
+                    path_copy = path.copy()
+                    path_copy.push(adj_rooms)
+                    s.push(path_copy)
+        return visited
+
+    results = dft
+    traversal_path.append(results)
 
 # TRAVERSAL TEST
 visited_rooms = set()
